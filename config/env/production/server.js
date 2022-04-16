cf1e71390d70cb06e42dcc296706f412fb3461fc
joelebukatobi <joelebuka@gmail.com>
2022-04-16 03:29:37 +0100

    Made Changes to the backend

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,9 +2,6 @@ module.exports = ({ env }) => ({
   proxy: true,
   url: env("MY_HEROKU_URL"),
   app: {
-    keys: env.array("APP_KEYS", [
-      "xKzoRR2mmeh72puK60W2ww==",
-      "siVDiquqeypkDuuzPYvYLQ==",
-    ]),
+    keys: env.array("APP_KEYS"),
   },
 });
